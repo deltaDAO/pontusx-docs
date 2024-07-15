2d098c61150b0fd72a8905c62b9edfd19b99ab7a
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2024-07-15 20:16:49 +0200

    docs: add technical architecture section

--- sidebar.ts
+++ sidebar.ts
@@ -1,35 +1,54 @@
-import type { Sidebar } from "vocs";
+import type { Sidebar } from 'vocs'
 
 export const sidebar = {
-  "/docs/": [
+  '/docs/': [
     {
-      text: "Introduction",
+      text: 'Introduction',
       items: [
-        { text: "Overview", link: "/docs/introduction/overview" },
+        { text: 'Overview', link: '/docs/introduction/overview' },
         {
-          text: "Vision, Mission and Values",
-          link: "/docs/introduction/vision-mission-values",
+          text: 'Vision, Mission and Values',
+          link: '/docs/introduction/vision-mission-values',
         },
         // { text: 'FAQ', link: '/docs/faq' }
       ],
     },
     {
-      text: "Pontus-X Networks",
+      text: 'Pontus-X Networks',
       items: [
-        { text: "Quick Start", link: "/docs/pontus-x-networks/quick-start" },
+        { text: 'Quick Start', link: '/docs/pontus-x-networks/quick-start' },
         {
-          text: "Validator Node Setup",
-          link: "/docs/pontus-x-networks/node-setup",
+          text: 'Validator Node Setup',
+          link: '/docs/pontus-x-networks/node-setup',
+        },
+      ],
+    },
+    {
+      text: 'Technical Architecture',
+      collapsed: true,
+      items: [
+        {
+          text: 'Overview',
+          link: '/docs/technical-architecture/architecture-overview',
+        },
+        {
+          text: 'Core Components',
+          items: [
+            {
+              text: 'Access Controller',
+              link: '/docs/technical-architecture/access-controller',
+            },
+          ],
         },
       ],
     },
 
     {
-      text: "Legal",
+      text: 'Legal',
       items: [
-        { text: "Privacy", link: "https://docs.pontus-x.eu/privacy" },
-        { text: "Imprint", link: "https://delta-dao.com/imprint" },
+        { text: 'Privacy', link: 'https://docs.pontus-x.eu/privacy' },
+        { text: 'Imprint', link: 'https://delta-dao.com/imprint' },
       ],
     },
   ],
-} as const satisfies Sidebar;
+} as const satisfies Sidebar
