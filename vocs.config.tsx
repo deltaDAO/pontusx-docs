02b4caa30c68ed7382af30301e2c47daaeffba58
Albert Peci <albert.peci@posteo.de>
2024-08-05 09:36:08 +0200

    docs: add portals to navbar

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -1,76 +1,94 @@
-import { defineConfig } from 'vocs'
-import pkg from './package.json'
-import { sidebar } from './sidebar'
-import remarkGemoji from 'remark-gemoji'
+import { defineConfig } from "vocs";
+import pkg from "./package.json";
+import { sidebar } from "./sidebar";
+import remarkGemoji from "remark-gemoji";
 
 export default defineConfig({
-  baseUrl: 'https://pontusx-docs.vercel.app',
-  title: 'Pontus-X Docs',
-  titleTemplate: '%s · Pontus-X Docs',
+  baseUrl: "https://pontusx-docs.vercel.app",
+  title: "Pontus-X Docs",
+  titleTemplate: "%s · Pontus-X Docs",
   description:
-    'The official documentation for Pontus-X, a decentralized data and ai ecosystem.',
-  head: (
-    <>
-    </>
-  ),
+    "The official documentation for Pontus-X, a decentralized data and ai ecosystem.",
+  head: <></>,
   ogImageUrl: {
-    '/': '/og-image.png',
-    '/docs':
-      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
-    '/op-stack':
-      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description'
+    "/": "/og-image.png",
+    "/docs":
+      "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
+    "/op-stack":
+      "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
   },
-  iconUrl: { light: '/favicons/icon.png', dark: '/favicons/icon.png' },
-  logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
+  iconUrl: { light: "/favicons/icon.png", dark: "/favicons/icon.png" },
+  logoUrl: { light: "/icon-light.png", dark: "/icon-dark.png" },
   rootDir: ".",
   sidebar,
   socials: [
     {
-      icon: 'github',
-      link: 'https://github.com/deltaDAO/pontusx-docs'
-    }
-  ],
-  sponsors: [
-    {
-      name: 'Enterprises',
-      height: 80,
-      items: [
-        [
-          {
-            name: 'OceanProtocol',
-            link: 'https://oceanprotocol.com',
-            image:
-              'https://oceanprotocol.com/static/ae84296f3b9ccb7054530d3af623f1fa/logo.svg'
-          },
-          {
-            name: 'Oasis Network',
-            link: 'https://oasisprotocol.org/',
-            image:
-              'https://assets-global.website-files.com/63617eb68a66008a1a2130a0/639b268fde36b82f6ca20500_Network%20Logo.svg'
-          }
-        ]
-      ]
-    }
+      icon: "github",
+      link: "https://github.com/deltaDAO/pontusx-docs",
+    },
   ],
   theme: {
     accentColor: {
-      light: '#004967',
-      dark: '#00a19e'
-    }
+      light: "#004967",
+      dark: "#00a19e",
+    },
   },
   topNav: [
-    { text: 'Docs', link: '/docs/getting-started', match: '/docs' },
     {
-      text: pkg.version,
+      text: "Portals",
       items: [
         {
-          text: 'Contributing',
-          link: 'https://github.com/deltaDAO/pontusx-docs/blob/main/.github/CONTRIBUTING.md',
+          text: "Pontus-X Portal",
+          link: "https://www.pontus-x.eu/",
         },
-      ]
-    }
+        {
+          text: "EuProGigant Portal",
+          link: "https://portal.euprogigant.io/",
+        },
+        {
+          text: "moveID Portal",
+          link: "https://portal.moveid.eu/",
+        },
+        {
+          text: "Cooperants Portal",
+          link: "https://cooperants.pontus-x.eu/",
+        },
+        {
+          text: "Airbus Portal",
+          link: "https://airbus.pontus-x.eu/",
+        },
+        {
+          text: "Service-Meister Portal",
+          link: "https://servicemeister.pontus-x.eu/",
+        },
+        {
+          text: "Berlin State Portal",
+          link: "https://sbb.pontus-x.eu/",
+        },
+        {
+          text: "Flex4Res Portal",
+          link: "https://flex4res.pontus-x.eu/",
+        },
+        {
+          text: "Future Mobility Portal",
+          link: "https://marketplace.future-mobility-alliance.org/",
+        },
+        {
+          text: "EnergyShr Portal",
+          link: "https://www.energyshr.nl/",
+        },
+        {
+          text: "EnergySHR Portal",
+          link: "https://www.energyshr.nl/",
+        },
+        {
+          text: "UdL Research Portal",
+          link: "https://dataspace.angliru.udl.cat/",
+        },
+      ],
+    },
   ],
   markdown: {
     remarkPlugins: [remarkGemoji],
-  }
-})
+  },
+});
