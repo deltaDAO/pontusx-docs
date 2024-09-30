772b8c400681f558b9b3b2099fc1db1b7ac7cf02
Sebastian Abromeit <38963270+Abrom8@users.noreply.github.com>
2024-09-30 11:23:58 +0200

    fix: use right head syntax in config (#38)

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -1,14 +1,15 @@
-import { defineConfig } from "vocs";
-import { sidebar } from "./sidebar";
-import remarkGemoji from "remark-gemoji";
+import { defineConfig } from 'vocs'
+import { sidebar } from './sidebar'
+import remarkGemoji from 'remark-gemoji'
 
 export default defineConfig({
-  baseUrl: "https://docs.pontus-x.eu",
-  title: "Pontus-X Docs",
-  titleTemplate: "%s · Pontus-X Docs",
+  baseUrl: 'https://docs.pontus-x.eu',
+  title: 'Pontus-X Docs',
+  titleTemplate: '%s · Pontus-X Docs',
   description:
-    "The official documentation for Pontus-X, a decentralized data and ai ecosystem.",
-    head: (
+    'The official documentation for Pontus-X, a decentralized data and ai ecosystem.',
+  head() {
+    return (
       <>
         <script
           defer
@@ -16,81 +17,82 @@ export default defineConfig({
           src='https://plausible.io/js/script.js'
         ></script>
       </>
-    ),
-  ogImageUrl: {
-    "/": "/og-image.png",
-    "/docs":
-      "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
-    "/op-stack":
-      "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
+    )
   },
-  iconUrl: { light: "/favicons/icon.png", dark: "/favicons/icon.png" },
-  logoUrl: { light: "/icon-light.png", dark: "/icon-dark.png" },
-  rootDir: ".",
+  ogImageUrl: {
+    '/': '/og-image.png',
+    '/docs':
+      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
+    '/op-stack':
+      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
+  },
+  iconUrl: { light: '/favicons/icon.png', dark: '/favicons/icon.png' },
+  logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
+  rootDir: '.',
   sidebar,
   socials: [
     {
-      icon: "github",
-      link: "https://github.com/deltaDAO/pontusx-docs",
+      icon: 'github',
+      link: 'https://github.com/deltaDAO/pontusx-docs',
     },
   ],
   theme: {
     accentColor: {
-      light: "#004967",
-      dark: "#00a19e",
+      light: '#004967',
+      dark: '#00a19e',
     },
   },
   topNav: [
     {
-      text: "Portals",
+      text: 'Portals',
       items: [
         {
-          text: "Pontus-X Portal",
-          link: "https://www.pontus-x.eu/",
+          text: 'Pontus-X Portal',
+          link: 'https://www.pontus-x.eu/',
         },
         {
-          text: "EuProGigant Portal",
-          link: "https://portal.euprogigant.io/",
+          text: 'EuProGigant Portal',
+          link: 'https://portal.euprogigant.io/',
         },
         {
-          text: "moveID Portal",
-          link: "https://portal.moveid.eu/",
+          text: 'moveID Portal',
+          link: 'https://portal.moveid.eu/',
         },
         {
-          text: "Cooperants Portal",
-          link: "https://federator.cooperants.info/marketplace",
+          text: 'Cooperants Portal',
+          link: 'https://federator.cooperants.info/marketplace',
         },
         {
-          text: "Airbus Portal",
-          link: "https://airbus.pontus-x.eu/",
+          text: 'Airbus Portal',
+          link: 'https://airbus.pontus-x.eu/',
         },
         {
-          text: "Service-Meister Portal",
-          link: "https://servicemeister.pontus-x.eu/",
+          text: 'Service-Meister Portal',
+          link: 'https://servicemeister.pontus-x.eu/',
         },
         {
-          text: "Berlin State Portal",
-          link: "https://sbb.pontus-x.eu/",
+          text: 'Berlin State Portal',
+          link: 'https://sbb.pontus-x.eu/',
         },
         {
-          text: "Flex4Res Portal",
-          link: "https://flex4res.pontus-x.eu/",
+          text: 'Flex4Res Portal',
+          link: 'https://flex4res.pontus-x.eu/',
         },
         {
-          text: "Future Mobility Portal",
-          link: "https://marketplace.future-mobility-alliance.org/",
+          text: 'Future Mobility Portal',
+          link: 'https://marketplace.future-mobility-alliance.org/',
         },
         {
-          text: "EnergyShr Portal",
-          link: "https://www.energyshr.nl/",
+          text: 'EnergyShr Portal',
+          link: 'https://www.energyshr.nl/',
         },
         {
-          text: "EnergySHR Portal",
-          link: "https://www.energyshr.nl/",
+          text: 'EnergySHR Portal',
+          link: 'https://www.energyshr.nl/',
         },
         {
-          text: "UdL Research Portal",
-          link: "https://dataspace.angliru.udl.cat/",
+          text: 'UdL Research Portal',
+          link: 'https://dataspace.angliru.udl.cat/',
         },
       ],
     },
@@ -98,4 +100,4 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkGemoji],
   },
-});
+})
